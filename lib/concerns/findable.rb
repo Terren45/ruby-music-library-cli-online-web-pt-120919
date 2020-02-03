d9ff7461994bcebbb136406a40106031c1d2bc17
Terren Culberson <culbersonterren@yahoo.com>
2020-02-03 21:35:13 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,5 +4,14 @@ module Conerns :: Findale
 			self.all.find{|item|item.name == name}
   end
   
-  
+  def find_or_create_by_name(name)
+			song = find_by_name(name)
+			if song == nil
+				self.create(name)
+			else
+				song
+			end
+		end
+
+	end
 end
